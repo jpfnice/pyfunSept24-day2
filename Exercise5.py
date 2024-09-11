d00eedfeb54ac1b655220e0a8edcbff35d27482d
jpfnice <jpfnice@gmail.com>
2024-09-11 16:21:02 +0200

    Add files via upload

--- Exercise5.py
+++ Exercise5.py
@@ -24,3 +24,27 @@ words=["He", "does", "confess", "he", "feels",
        "himself","distracted", "but", 
        "from", "what", "cause", "he", "will", 
        "by","no", "means", "speak"]
+
+aDict=dict() # an empty dict
+
+# This loop is used to analyse each element of the list 
+# words in turn:
+    
+for w in words: 
+    
+    if len(w) in aDict: # There is already an element in the dict with this length
+        aDict[len(w)].append(w)
+    else: # A new pair needs to be added to the dict
+        aDict[len(w)]=[w]
+
+# Let see what the dict looks like:
+        
+for k,v in aDict.items():
+    print(k, v)
+
+print("-"*30)    
+
+# or, to obtain a sorted list of pairs:
+
+for k in sorted(aDict.keys()):
+    print(k, aDict[k])
